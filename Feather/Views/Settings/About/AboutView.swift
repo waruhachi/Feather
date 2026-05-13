aee2df382c288380f7e875d6538f416e1329e0de
CLARATION <claration@riseup.net>
2026-05-12 21:30:13 -0700

    fix: llsc12

--- Feather/Views/Settings/About/AboutView.swift
+++ Feather/Views/Settings/About/AboutView.swift
@@ -23,7 +23,7 @@ struct AboutView: View {
 	@State private var _credits: [CreditsModel] = [
 		.init(name: "C", desc: "Developer", github: "claration"),
 		.init(name: "Asami", desc: "Developer", github: "Nyasami"),
-		.init(name: "Lakhan Lothiyi", desc: "AltStore Repositories", github: "llsc"),
+		.init(name: "Lakhan Lothiyi", desc: "AltStore Repositories", github: "llsc12"),
 	]
 	
 	let pngURL = URL(string: "https://sponsors.claration.dev/sponsors.png")!
